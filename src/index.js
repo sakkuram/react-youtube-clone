c069fa91e681190b38b65ec49ab6580eeb689a30
[den] <114509752+sakkuram@users.noreply.github.com>
2023-11-28 18:05:14 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import App from "./App";1
+import App from "./App";12
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
