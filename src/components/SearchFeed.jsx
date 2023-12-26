aa4214fd0bf231293fa74dea8e376cf49cfe9063
[den] <114509752+sakkuram@users.noreply.github.com>
2023-12-26 15:18:27 +0200

    Update SearchFeed.jsx

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -28,7 +28,7 @@ const SearchFeed = () => {
         <span style={{ color: "#FC1503" }}>{searchTerm}</span> videos
       </Typography>
 
-      <Videos videos={videos} />12
+      <Videos videos={videos} />123
     </Box>
   );
 };
